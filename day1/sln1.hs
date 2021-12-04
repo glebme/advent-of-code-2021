ef0979163590dadb919cfa37d0bc2cd19e2751fe
Tommy Truong <glebme@yahoo.com>
2021-12-04 23:12:35 +1100

    Day 1 - Part 2

--- day1/sln1.hs
+++ day1/sln1.hs
@@ -6,8 +6,9 @@ main = do
     (fileName:args) <- getArgs
     withFile fileName ReadMode (\handle -> do
         contents <- hGetContents handle
-        let depths = lines contents
-        putStrLn . show . numberOfAscendingSequences $ map read depths)
+        let depths = map read $ lines contents
+        putStrLn . show $ numberOfAscendingSequences depths
+        putStrLn . show . numberOfAscendingSequences $ rollingWindowSequences depths)
 
 numberOfAscendingSequences :: [Int] -> Int
 numberOfAscendingSequences = pairWiseCompare . reverse
@@ -15,3 +16,6 @@ numberOfAscendingSequences = pairWiseCompare . reverse
 pairWiseCompare :: [Int] -> Int
 pairWiseCompare (x:y:ys) = if x > y then 1 + pairWiseCompare (y:ys) else pairWiseCompare(y:ys)
 pairWiseCompare _ = 0
+
+rollingWindowSequences :: [Int] -> [Int]
+rollingWindowSequences xs = foldl (zipWith (+)) xs [(tail xs), tail (tail xs)]
