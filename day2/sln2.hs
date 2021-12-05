b567c3ec74ffc0d62748126578339b2d08a32ffa
Tommy Truong <glebme@yahoo.com>
2021-12-05 15:54:22 +1100

    Day 2 - Part 2

--- day2/sln2.hs
+++ day2/sln2.hs
@@ -1,26 +1,32 @@
 import System.Environment
 import System.IO
 
-data Coordinate = Coordinate Int Int
+data Coordinate = Part1 Int Int | Part2 Int Int Int
 instance Show Coordinate where
-    show (Coordinate x d) = concat ["Horizontal position is ", show x, ".\nDepth is ", show d, ".\nMultiplying: ", show (x * d)]
+    show (Part1 x d) = concat ["Horizontal position is ", show x, ".\nDepth is ", show d, ".\nMultiplying: ", show (x * d)]
+    show (Part2 x d a) = concat ["Horizontal position is ", show x, ".\nDepth is ", show d, ".\nAim is ", show a, ".\nMultiplying: ", show (x * d)]
 
 main :: IO ()
 main = do
     (fileName: args) <- getArgs
     withFile fileName ReadMode (\handle -> do
         content <- hGetContents handle
-        let instructions = lines content
-        putStrLn . show . finalPosition $ parseInstructions instructions)
+        let instructions = parseInstructions $ lines content
+        putStrLn . show $ finalPosition (Part1 0 0) instructions
+        putStrLn . show $ finalPosition (Part2 0 0 0) instructions)
 
 parseInstructions :: [String] -> [(String, Int)]
-parseInstructions instructions =  map ((\[x,y] -> (x, read y)) . words) instructions
-    
-finalPosition :: [(String, Int)] -> Coordinate
-finalPosition xs = foldl (\position newInstruction -> addInstructions position newInstruction) (Coordinate 0 0) xs
+parseInstructions = map ((\[x,y] -> (x, read y)) . words)
+
+finalPosition :: Coordinate -> [(String, Int)] -> Coordinate
+finalPosition startingPosition xs = foldl (\position newInstruction -> addInstructions position newInstruction) startingPosition xs
 
 addInstructions ::  Coordinate -> (String, Int) -> Coordinate
-addInstructions (Coordinate x d) (direction, distance) | direction == "forward" = Coordinate (x + distance) d
-                                                       | direction == "up" = Coordinate x (d - distance)
-                                                       | direction == "down" = Coordinate x (d + distance)
-                                                       | otherwise = undefined
+addInstructions (Part1 x d) (direction, value)   | direction == "forward" = Part1 (x + value) d
+                                                 | direction == "up" = Part1 x (d - value)
+                                                 | direction == "down" = Part1 x (d + value)
+                                                 | otherwise = undefined
+addInstructions (Part2 x d a) (direction, value) | direction == "forward" = Part2 (x + value) (d + (a*value)) a
+                                                 | direction == "up" = Part2 x d (a - value)
+                                                 | direction == "down" = Part2 x d (a + value)
+                                                 | otherwise = undefined
