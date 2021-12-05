faf7eff50366e1b49635f3042552ea5e0e7da034
Tommy Truong <glebme@yahoo.com>
2021-12-05 21:28:13 +1100

    Day 3 - Part 2

--- day3/day3.hs
+++ day3/day3.hs
@@ -10,15 +10,17 @@ main = do
     withFile filename ReadMode (\handle -> do
         content <- hGetContents handle
         let report = lines content
-        putStrLn . show $ getPowerConsumption report)
+        putStrLn . show $ co2Rating report 0
+        putStrLn . show $ getPowerConsumption report
+        putStrLn . show $ getLifeSupportRating report)
 
 getPowerConsumption :: [String] -> Int
 getPowerConsumption report = gammaRate * epsilonRate
     where
-        [gammaRate, epsilonRate] = map convertBinaryStringToInt $ generateBinaryRates report
+        [gammaRate, epsilonRate] = map convertBinaryStringToInt $ generatePowerBinaryRates report
 
-generateBinaryRates :: [String] -> [String]
-generateBinaryRates xs = transpose $ map (nub .concat . sortListOfListBasedOnLength . group . sort) $ transpose xs
+generatePowerBinaryRates :: [String] -> [String]
+generatePowerBinaryRates xs = transpose $ map (nub .concat . sortListOfListBasedOnLength . group . sort) $ transpose xs
 
 sortListOfListBasedOnLength :: [[a]] -> [[a]]
 sortListOfListBasedOnLength = sortBy (compare `on` length)
@@ -29,4 +31,28 @@ convertBinaryStringToInt (x:xs) | x == '0' = convertBinaryStringToInt xs
                                 | x == '1' = 2 ^ (length xs) + convertBinaryStringToInt xs
                                 | otherwise = undefined
 
+getLifeSupportRating :: [String] -> Int
+getLifeSupportRating report = (convertBinaryStringToInt $ oxygenRating report 0) * (convertBinaryStringToInt $ co2Rating report 0)
 
+oxygenRating :: [String] -> Int -> String
+oxygenRating [x] _ = x
+oxygenRating xs position | length zeros == 0 && length ones == 0 = ""
+                         | length zeros > length ones = oxygenRating zeros (position+1)
+                         | length zeros <= length ones = oxygenRating ones (position+1)
+    where
+        (zeros, ones) = headFilter xs position 
+
+co2Rating :: [String] -> Int -> String
+co2Rating [x] _ = x
+co2Rating xs position | length zeros == 0 && length ones == 0 = ""
+                      | length zeros <= length ones = co2Rating zeros (position+1)
+                      | length zeros > length ones = co2Rating ones (position+1)
+    where
+        (zeros, ones) = headFilter xs position 
+
+
+headFilter :: [String] -> Int -> ([String], [String])
+headFilter [] _ = ([], [])
+headFilter xs@(y:ys) position = if (length y == position)
+    then ([],[])
+    else partition (\value -> if (value!!position) == '0' then True else False) xs 
